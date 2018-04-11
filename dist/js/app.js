8fc722d7abb3c0640f88b1e8077454bc75a46a6c
Agnieszka Osowska <agnieszta15@go2.pl>
2018-04-11 15:35:53 +0200

    zm

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,37 +8,87 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  *Stwórz interfejs w HTML, który pozwoli na oddawanie głosów (oraz będzie wyświetlał wyniki). */
 
 var Ankieta = function () {
-  function Ankieta() {
-    _classCallCheck(this, Ankieta);
+    function Ankieta() {
+        _classCallCheck(this, Ankieta);
 
-    this.question = 'Czy lubisz kolor zielony?';
-  }
-
-  _createClass(Ankieta, [{
-    key: 'tak',
-    value: function tak() {
-      var place = document.getElementById('odpowiedz');
-      place.innerHTML = 'Lubisz kolor zielony';
+        this.question = 'Czy lubisz kolor zielony?';
     }
-  }, {
-    key: 'nie',
-    value: function nie() {
-      var place = document.getElementById('odpowiedz');
-      place.innerHTML = 'Nie lubisz kolor zielonego';
-    }
-  }]);
 
-  return Ankieta;
+    _createClass(Ankieta, [{
+        key: 'tak',
+        value: function tak() {
+            document.getElementById('tak').checked = true;
+            var place = document.getElementById('odpowiedz');
+            place.innerHTML = 'Lubisz kolor zielony';
+        }
+    }, {
+        key: 'nie',
+        value: function nie() {
+            document.getElementById('nie').checked = true;
+            var place = document.getElementById('odpowiedz');
+            place.innerHTML = 'Nie lubisz koloru zielonego';
+        }
+    }]);
+
+    return Ankieta;
 }();
 
 var ankieta1 = new Ankieta();
 
 var yes = document.getElementById('tak');
 yes.addEventListener('click', function () {
-  ankieta1.tak();
+    ankieta1.tak();
 });
 var no = document.getElementById('nie');
 no.addEventListener('click', function () {
-  ankieta1.nie();
+    ankieta1.nie();
 });
+
+/* 3.Zaprojektuj i stwórz obiektową reprezentację automatu do kawy. Automat pozwala zamówić dużą lub małą kawę. 
+Do każdej kawy można wziąć jedną lub więcej porcji mleka lub/i cukru. Po wybraniu zamówienia, powinna być zwracana informacja o cenie lub o niepowodzeniu, 
+jeśli nie udało sie go zrealizować z powodu braku składników. Cenę małej i dużej kawy oraz pojedynczej porcji mleka i cukru ustalamy tworząc obiekt. 
+Automat przechowuje pewną ilość składników: mielonej kawy oraz porcji mleka i cukru. Dodawać je możemy przy pomocy osobnych metod. Mleko i cukier ładowane są w porcjach, 
+natomiast kawa w gramach. Przygotowanie małej kawy zużywa 6 gramów, natomiast dużej - 15.
+*Stwórz interfejs w HTML, który pozwoli skorzystać z automatu (a także doładowywać go składnikami). */
+
+var Automat = function () {
+    function Automat(priceSmallCoffee, priceLargeCoffee, priceSugar, priceMilk) {
+        _classCallCheck(this, Automat);
+
+        this.priceSmallCoffee = priceSmallCoffee;
+        this.priceLargeCoffee = priceLargeCoffee;
+        this.priceSugar = priceSugar;
+        this.priceMilk = priceMilk;
+        this.amountSugar = 4;
+        this.amountMilk = 4;
+        this.amountCoffee = 21;
+    }
+
+    _createClass(Automat, [{
+        key: 'orderSmallCoffee',
+        value: function orderSmallCoffee(sugar, milk) {
+            var messagePleace = document.getElementById('message');
+            var pricePleace = document.getElementById('price');
+            switch (sugar, milk) {
+                case sugar > this.amountSugar:
+                    messagePleace.innerHTML = 'Automat nie ma wystarczającej ilości cukru';
+                    break;
+                case milk > this.amountMilk:
+                    messagePleace.innerHTML = 'Automat nie ma wystarczającej ilości mleka';
+                    break;
+                default:
+                    this.amountSugar -= sugar;
+                    this.amountMilk -= milk;
+                    this.amountCoffee -= 6;
+                    var totalPrice = sugar * this.priceSugar + milk * this.priceMilk + this.priceSmallCoffee;
+                    pricePlace.innerHTML = totalPrice;
+            }
+        }
+    }]);
+
+    return Automat;
+}();
+
+var automat1 = new Automat(6, 9, 2, 3);
+automat1.orderSmallCoffee(2, 2);
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,14 @@
          this.question = 'Czy lubisz kolor zielony?';
      }
      tak() {
-             const place = document.getElementById('odpowiedz');
-             place.innerHTML = 'Lubisz kolor zielony';
+        document.getElementById('tak').checked = true;
+        const place = document.getElementById('odpowiedz');
+        place.innerHTML = 'Lubisz kolor zielony';
      }
      nie() {
-            const place = document.getElementById('odpowiedz');
-            place.innerHTML = 'Nie lubisz kolor zielonego';
+        document.getElementById('nie').checked = true;
+        const place = document.getElementById('odpowiedz');
+        place.innerHTML = 'Nie lubisz koloru zielonego';
     }
  }
 
@@ -26,3 +28,43 @@
      ankieta1.nie();
  })
  
+
+ /* 3.Zaprojektuj i stwórz obiektową reprezentację automatu do kawy. Automat pozwala zamówić dużą lub małą kawę. 
+ Do każdej kawy można wziąć jedną lub więcej porcji mleka lub/i cukru. Po wybraniu zamówienia, powinna być zwracana informacja o cenie lub o niepowodzeniu, 
+ jeśli nie udało sie go zrealizować z powodu braku składników. Cenę małej i dużej kawy oraz pojedynczej porcji mleka i cukru ustalamy tworząc obiekt. 
+ Automat przechowuje pewną ilość składników: mielonej kawy oraz porcji mleka i cukru. Dodawać je możemy przy pomocy osobnych metod. Mleko i cukier ładowane są w porcjach, 
+ natomiast kawa w gramach. Przygotowanie małej kawy zużywa 6 gramów, natomiast dużej - 15.
+ *Stwórz interfejs w HTML, który pozwoli skorzystać z automatu (a także doładowywać go składnikami). */
+
+class Automat {
+    constructor(priceSmallCoffee, priceLargeCoffee, priceSugar, priceMilk) {
+        this.priceSmallCoffee = priceSmallCoffee;
+        this.priceLargeCoffee = priceLargeCoffee;
+        this.priceSugar = priceSugar;
+        this.priceMilk = priceMilk;
+        this.amountSugar = 4;
+        this.amountMilk = 4;
+        this.amountCoffee = 21;
+    }
+    /*orderSmallCoffee(sugar, milk) {
+        switch(sugar, milk) {
+            case (sugar > this.amountSugar):
+            const messagePleace = document.getElementById('message');
+
+            break;
+            case (milk > this.amountMilk):
+            const messagePleace = document.getElementById('message');
+
+            break;
+            default:
+            let totalPrice = sugar * this.priceSugar + milk * this.priceMilk + this.priceSmallCoffee;
+            console.log(totalPrice);
+            this.amountSugar -= sugar;
+            this.amountMilk -= milk;
+            this.amountCoffee -= 6;
+        }
+    }
+}
+
+let automat1 = new Automat(6, 9, 2, 3);
+automat1.orderSmallCoffee(2, 2);*/
